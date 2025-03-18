800ff915a029443b0f1c7d51422f6cd848cf2be3
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-18 12:02:04 +0530

    Create SpringBootRestApiService2Application .java

---
+++ SpringBootRestAPIService2/src/main/java/com.test/SpringBootRestApiService2Application .java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiService2Application.class, args);
+	}
+
+}
